1149fd3f3946b5d9ebf84d88fc00162b71582420
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-16 23:47:05 -0500

    parameterize versions

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,8 @@
 
 concourse_version=${concourse_version:-0.45.0}
 garden_version=${garden_version:-0.190.0}
+aws_cpi_version=${aws_cpi_version:-5}
+stemcell_version=${stemcell_version:-2830}
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
@@ -9,7 +11,7 @@ cd $DIR/..
 export PATH=$PATH:$PWD/bin
 
 bosh-init deploy concourse.yml \
-  assets/light-bosh-stemcell-2830-aws-xen-ubuntu-trusty-go_agent.tgz \
-  assets/bosh-aws-cpi-release-5.tgz \
+  assets/light-bosh-stemcell-${stemcell_version}-aws-xen-ubuntu-trusty-go_agent.tgz \
+  assets/bosh-aws-cpi-release-${aws_cpi_version}.tgz \
   assets/concourse-${concourse_version}.tgz \
   assets/garden-linux-release-${garden_version}.tgz
